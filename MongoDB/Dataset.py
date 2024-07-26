288cd654a7a4f1edadb1826f7f890adaf4f3c1ba
Gianniz03 <zullo.giovanni03@gmail.com>
2024-07-26 14:56:23 +0200

    aggiunto ubo
    Co-Authored-By: N1ntr1x <166445609+N1ntr1x@users.noreply.github.com>

--- MongoDB/Dataset.py
+++ MongoDB/Dataset.py
@@ -6,12 +6,14 @@ fake = Faker()
 
 # Definizione delle costanti
 NUM_COMPANIES = 10000
+NUM_UBO = 1000  # Numero di UBO da generare
 
 # Genera nomi di forma giuridica
 forme_giuridiche = ['S.r.l.', 'S.p.A.', 'S.a.S.', 'S.n.C.', 'S.r.l. a socio unico', 'Cooperative', 'Onlus']
 
 # Inizializza la lista per tracciare gli ID già assegnati
 used_company_ids = set()
+used_ubo_ids = set()
 
 # Creazione della collezione 'Aziende'
 with open('Dataset/companies.csv', 'w', newline='') as csvfile:
@@ -40,3 +42,30 @@ with open('Dataset/companies.csv', 'w', newline='') as csvfile:
         used_company_ids.add(company_id)
 
 print("File CSV 'dataset_companies.csv' creato con successo.")
+
+# Creazione della collezione 'Proprietari Benefici Ultimi (UBO)'
+with open('Dataset/ubo.csv', 'w', newline='') as csvfile:
+    fieldnames = ['id', 'nome', 'indirizzo', 'data_di_nascita', 'nazionalità', 'percentuale_partecipazione', 'tipo']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for ubo_id in range(1, NUM_UBO + 1):
+        name = fake.name()
+        address = fake.address()
+        birthdate = fake.date_of_birth()
+        nationality = fake.country()
+        ownership_percentage = round(random.uniform(0.1, 100.0), 2)
+        ubo_type = random.choice(['Persona', 'Azienda'])
+
+        writer.writerow({
+            'id': ubo_id,
+            'nome': name,
+            'indirizzo': address,
+            'data_di_nascita': birthdate,
+            'nazionalità': nationality,
+            'percentuale_partecipazione': ownership_percentage,
+            'tipo': ubo_type
+        })
+        used_ubo_ids.add(ubo_id)
+
+print("File CSV 'dataset_ubo.csv' creato con successo.")
