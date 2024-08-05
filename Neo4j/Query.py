4906b865af99e6dcb9f593b808dc83cabc2e5cda
N1ntr1x <166445609+N1ntr1x@users.noreply.github.com>
2024-08-05 18:04:20 +0200

    Update
    LOL
    
    Co-Authored-By: Gianniz03 <166447659+Gianniz03@users.noreply.github.com>

--- Neo4j/Query.py
+++ Neo4j/Query.py
@@ -12,10 +12,10 @@ graph25 = Graph("bolt://localhost:7687", user="neo4j", password="12345678", name
 
 def calculate_confidence_interval(data, confidence=0.95):
     n = len(data)
-    mean_value = np.mean(data)
+    average_value = np.average(data)
     stderr = stats.sem(data)
     margin_of_error = stderr * stats.t.ppf((1 + confidence) / 2, n - 1)
-    return mean_value, margin_of_error
+    return average_value, margin_of_error
 
 def measure_query_performance(graph, query_func, percentage, iterations=30):
     subsequent_times = []
@@ -27,10 +27,10 @@ def measure_query_performance(graph, query_func, percentage, iterations=30):
         execution_time = (end_time - start_time) * 1000  # Milliseconds
         subsequent_times.append(execution_time)
     
-    mean, margin_of_error = calculate_confidence_interval(subsequent_times)
+    average, margin_of_error = calculate_confidence_interval(subsequent_times)
     average_subsequent_time = round(sum(subsequent_times) / len(subsequent_times), 2)
     
-    return average_subsequent_time, mean, margin_of_error
+    return average_subsequent_time, average, margin_of_error
 
 def query1(graph):
     company_name = 'Pittman Ltd'
@@ -107,10 +107,10 @@ def main():
         print(f"Response time (first execution - Query 1): {time_first_execution} ms")
         response_times_first_execution[f"{percentage} - Query 1"] = time_first_execution
 
-        average_subsequent_time, mean, margin_of_error = measure_query_performance(graph, query1, percentage)
+        average_subsequent_time, average, margin_of_error = measure_query_performance(graph, query1, percentage)
         print(f"Average time of 30 subsequent executions (Query 1): {average_subsequent_time} ms")
-        print(f"Confidence Interval (Query 1): [{round(mean - margin_of_error, 2)}, {round(mean + margin_of_error, 2)}] ms\n")
-        average_response_times[f"{percentage} - Query 1"] = (average_subsequent_time, mean, margin_of_error)
+        print(f"Confidence Interval (Query 1): [{round(average - margin_of_error, 2)}, {round(average + margin_of_error, 2)}] ms\n")
+        average_response_times[f"{percentage} - Query 1"] = (average_subsequent_time, average, margin_of_error)
 
         # Query 2
         start_time = time.time()
@@ -126,10 +126,10 @@ def main():
         print(f"Response time (first execution - Query 2): {time_first_execution} ms")
         response_times_first_execution[f"{percentage} - Query 2"] = time_first_execution
 
-        average_subsequent_time, mean, margin_of_error = measure_query_performance(graph, query2, percentage)
+        average_subsequent_time, average, margin_of_error = measure_query_performance(graph, query2, percentage)
         print(f"Average time of 30 subsequent executions (Query 2): {average_subsequent_time} ms")
-        print(f"Confidence Interval (Query 2): [{round(mean - margin_of_error, 2)}, {round(mean + margin_of_error, 2)}] ms\n")
-        average_response_times[f"{percentage} - Query 2"] = (average_subsequent_time, mean, margin_of_error)
+        print(f"Confidence Interval (Query 2): [{round(average - margin_of_error, 2)}, {round(average + margin_of_error, 2)}] ms\n")
+        average_response_times[f"{percentage} - Query 2"] = (average_subsequent_time, average, margin_of_error)
 
         # Query 3
         start_time = time.time()
@@ -145,10 +145,10 @@ def main():
         print(f"Response time (first execution - Query 3): {time_first_execution} ms")
         response_times_first_execution[f"{percentage} - Query 3"] = time_first_execution
 
-        average_subsequent_time, mean, margin_of_error = measure_query_performance(graph, query3, percentage)
+        average_subsequent_time, average, margin_of_error = measure_query_performance(graph, query3, percentage)
         print(f"Average time of 30 subsequent executions (Query 3): {average_subsequent_time} ms")
-        print(f"Confidence Interval (Query 3): [{round(mean - margin_of_error, 2)}, {round(mean + margin_of_error, 2)}] ms\n")
-        average_response_times[f"{percentage} - Query 3"] = (average_subsequent_time, mean, margin_of_error)
+        print(f"Confidence Interval (Query 3): [{round(average - margin_of_error, 2)}, {round(average + margin_of_error, 2)}] ms\n")
+        average_response_times[f"{percentage} - Query 3"] = (average_subsequent_time, average, margin_of_error)
 
         # Query 4
         start_time = time.time()
@@ -164,10 +164,10 @@ def main():
         print(f"Response time (first execution - Query 4): {time_first_execution} ms")
         response_times_first_execution[f"{percentage} - Query 4"] = time_first_execution
 
-        average_subsequent_time, mean, margin_of_error = measure_query_performance(graph, query4, percentage)
+        average_subsequent_time, average, margin_of_error = measure_query_performance(graph, query4, percentage)
         print(f"Average time of 30 subsequent executions (Query 4): {average_subsequent_time} ms")
-        print(f"Confidence Interval (Query 4): [{round(mean - margin_of_error, 2)}, {round(mean + margin_of_error, 2)}] ms\n")
-        average_response_times[f"{percentage} - Query 4"] = (average_subsequent_time, mean, margin_of_error)
+        print(f"Confidence Interval (Query 4): [{round(average - margin_of_error, 2)}, {round(average + margin_of_error, 2)}] ms\n")
+        average_response_times[f"{percentage} - Query 4"] = (average_subsequent_time, average, margin_of_error)
 
   
     with open('Neo4j/ResponseTimes/neo4j_times_of_response_first_execution.csv', mode='w', newline='') as file:
@@ -179,10 +179,10 @@ def main():
 
     with open('Neo4j/ResponseTimes/neo4j_response_times_average_30.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Dataset", "Query", "Milliseconds", "Mean", "Confidence Interval (Min, Max)"])
-        for key, (average_time, mean, margin_of_error) in average_response_times.items():
+        writer.writerow(["Dataset", "Query", "Milliseconds", "Average", "Confidence Interval (Min, Max)"])
+        for key, (average_time, average, margin_of_error) in average_response_times.items():
             percentage, query = key.split(' - ')
-            writer.writerow([percentage, query, average_time, round(mean, 2), f"[{round(mean - margin_of_error, 2)}, {round(mean + margin_of_error, 2)}]"])
+            writer.writerow([percentage, query, average_time, round(average, 2), f"[{round(average - margin_of_error, 2)}, {round(average + margin_of_error, 2)}]"])
 
 if __name__ == "__main__":
     main()
