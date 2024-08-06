301bf262f8ef46cae9c16355132289df8a98a31f
Gianniz03 <zullo.giovanni03@gmail.com>
2024-08-06 17:25:15 +0200

    Update
    LollOL

--- RunBenchmark.py
+++ RunBenchmark.py
@@ -3,19 +3,19 @@ import subprocess
 def execute_scripts():
     try:
         # Esegui il primo script
-        print(f"Script MongoDB/Query.py in esecuzione")
+        print(f"Running MongoDB/Query.py script")
         subprocess.run(['python', 'MongoDB/Query.py'], check=True)
 
         # Esegui il secondo script
-        print(f"Script Neo4j/Query.py in esecuzione")
+        print(f"Running Neo4j/Query.py script")
         subprocess.run(['python', 'Neo4j/Query.py'], check=True)
 
         # Esegui il terzo script
-        print(f"Script Histograms/GenerateHistograms.py in esecuzione")
+        print(f"Running Histograms/GenerateHistograms.py script")
         subprocess.run(['python', 'Histograms/GenerateHistograms.py'], check=True)
 
     except subprocess.CalledProcessError as e:
-        print(f"Errore durante l'esecuzione di uno script: {e}")
+        print(f"Error occurred while executing a script: {e}")
 
 if __name__ == '__main__':
     execute_scripts()
